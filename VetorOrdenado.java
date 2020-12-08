967e2686b6bbfe2c6179a907c55fa2008880e5c9
Lucas Fell <lucasafell@gmail.com>
2020-12-08 19:21:22 -0300

    sorting, implementations

--- VetorOrdenado.java
+++ VetorOrdenado.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 public class VetorOrdenado implements Map<Integer, Estudante> {
@@ -13,6 +16,39 @@ public class VetorOrdenado implements Map<Integer, Estudante> {
     nElements = 0;
   }
 
+  public void sort(){
+    Integer[] auxKeys= new Integer[keys.length];
+    auxKeys = keys;
+    int aux;
+    for(int i=0; i<nElements; i++){
+      for(int j=0; j<nElements-1; j++){
+         if(auxKeys[j] > auxKeys[j+1]){
+            aux = auxKeys[j];
+            auxKeys[j] = auxKeys[j+1];
+            auxKeys[j+1] = aux;
+         }
+      }
+    }
+
+    Estudante[] newValues = new Estudante[values.length];
+    for(int i=0; i<nElements; i++){
+      newValues[i]=get(auxKeys[i]);
+    }
+
+    this.keys = auxKeys;
+    this.values = newValues;
+  }
+
+  public int countEngSoft(){
+    int count=0;
+    for(int i=0; i<nElements; i++){
+      if(values[i].getCurso().equals("Engenharia de Software")){
+        count++;
+      }
+    }
+    return count;
+  }
+
   @Override
   public int size() {
     return nElements;
@@ -20,25 +56,39 @@ public class VetorOrdenado implements Map<Integer, Estudante> {
 
   @Override
   public boolean isEmpty() {
-    // TODO Auto-generated method stub
-    return false;
+    return nElements == 0;
   }
 
   @Override
   public boolean containsKey(Object key) {
-    // TODO Auto-generated method stub
+    for(int k : keys) {
+      if(k == (int) key)
+        return true;
+    }
     return false;
   }
 
   @Override
   public boolean containsValue(Object value) {
-    // TODO Auto-generated method stub
+    for(Estudante e : values) {
+      if(e.compareTo((Estudante) value) == 0)
+        return true;
+    }
     return false;
+  }
+
+  public void printAll(){
+    for(int i=0; i<nElements; i++){
+      System.out.println("Chave: "+keys[i]+".  Valor: "+get(keys[i]));
+    }
   }
 
   @Override
   public Estudante get(Object key) {
-    // TODO Auto-generated method stub
+    for(Estudante e : values) {
+      if((int) key == e.getMatricula())
+        return e;
+    }
     return null;
   }
 
@@ -48,14 +98,37 @@ public class VetorOrdenado implements Map<Integer, Estudante> {
       keys[nElements] = key;
       values[nElements] = value;
       nElements++;
+      return value;
     }
-    return value;
+    return null;
   }
 
   @Override
   public Estudante remove(Object key) {
-    // TODO Auto-generated method stub
+    for(int i = 0; i < size(); i++) {
+      if((int) key == values[i].getMatricula()) {
+        Estudante e = values[i];
+        values[i] = null;
+        keys[i] = null;
+        nElements--;
+        return e;
+      }
+    }
     return null;
+  }
+
+  public void removeUntil(int key) {
+    for(int i = 0; i < size(); i++) {
+      if(values[i].getMatricula() > key)
+        break;
+      else {
+        values[i] = null;
+        keys[i] = null;
+        nElements--;
+      }
+    }
+    keys = Arrays.stream(keys).filter(Objects::nonNull).toArray(Integer[]::new);
+    values = Arrays.stream(values).filter(Objects::nonNull).toArray(Estudante[]::new);
   }
 
   @Override
@@ -66,14 +139,20 @@ public class VetorOrdenado implements Map<Integer, Estudante> {
 
   @Override
   public void clear() {
-    // TODO Auto-generated method stub
-
+    for(int i = 0; i < size(); i++) {
+      values[i] = null;
+      keys[i] = null;
+      nElements--;
+    }
   }
 
   @Override
   public Set<Integer> keySet() {
-    // TODO Auto-generated method stub
-    return null;
+    Set<Integer> set = new HashSet<>();
+    for(int i = 0; i < size(); i++) {
+      set.add(keys[i]);
+    }
+    return set;
   }
 
   @Override
